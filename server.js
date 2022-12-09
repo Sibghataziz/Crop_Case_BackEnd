dcc119b80c53cddfb69ccc5a46a8473af1b1e766
sibghataziz <sibghat.aziz98@gmail.com>
2022-12-09 17:12:48 +0530

    Try Catch added

--- server.js
+++ server.js
@@ -11,8 +11,8 @@ server.use(cors());
 server.get("/", (req, res) => {
   try {
     return res.status(200).json({
-        message: "Thank you for visiting this website",
-      });
+      message: "Thank you for visiting this website",
+    });
   } catch (error) {
     console.log(error);
   }
@@ -21,36 +21,35 @@ server.get("/", (req, res) => {
 server.get("/barChart", (req, res) => {
   try {
     const data = [];
-  const week = ["S","M","T","W","T","F","S"]
-  for (let i = 1; i <= 7; i++) {
-    const obj = {
-      name: week[i-1],
-      pre: Math.floor(Math.random() * 1000),
-      post: Math.floor(Math.random() * 1000),
-    };
-    data.push(obj);
-  }
+    const week = ["S", "M", "T", "W", "T", "F", "S"];
+    for (let i = 1; i <= 7; i++) {
+      const obj = {
+        name: week[i - 1],
+        pre: Math.floor(Math.random() * 1000),
+        post: Math.floor(Math.random() * 1000),
+      };
+      data.push(obj);
+    }
 
-  return res.status(200).json(data);
+    return res.status(200).json(data);
   } catch (error) {
     console.log(error);
   }
 });
 
 server.get("/lineChart", (req, res) => {
-  
   try {
     const data = [];
 
-  for (let i = 1; i <= 16; i++) {
-    const obj = {
-      name: `Nov ${i}`,
-      income: Math.floor(Math.random() * 1000),
-    };
-    data.push(obj);
-  }
+    for (let i = 1; i <= 16; i++) {
+      const obj = {
+        name: `Nov ${i}`,
+        income: Math.floor(Math.random() * 1000),
+      };
+      data.push(obj);
+    }
 
-  return res.status(200).json(data);
+    return res.status(200).json(data);
   } catch (error) {
     console.log(error);
   }
@@ -59,36 +58,36 @@ server.get("/lineChart", (req, res) => {
 server.get("/recent", (req, res) => {
   try {
     const name = ["Google Pay", "Amazon Pay", "PhonePay", "Twitter Pay"];
-  const month = [
-    "Jan",
-    "Feb",
-    "Mar",
-    "Apr",
-    "May",
-    "Jun",
-    "Jul",
-    "Aug",
-    "Sept",
-    "Oct",
-    "Nov",
-    "Dec",
-  ];
-  const data = [];
-  for (let i = 1; i <= 4; i++) {
-    const day = new Date();
-    const obj = {
-      name: name[i - 1],
-      price: Math.floor(Math.random() * 100),
-      date: `${day.getDate()} ${
-        month[day.getMonth()]
-      } ${day.getFullYear()} ${day.getHours()}:${day.getMinutes()}`,
-    };
-    data.push(obj);
-  }
+    const month = [
+      "Jan",
+      "Feb",
+      "Mar",
+      "Apr",
+      "May",
+      "Jun",
+      "Jul",
+      "Aug",
+      "Sept",
+      "Oct",
+      "Nov",
+      "Dec",
+    ];
+    const data = [];
+    for (let i = 1; i <= 4; i++) {
+      const day = new Date();
+      const obj = {
+        name: name[i - 1],
+        price: Math.floor(Math.random() * 100),
+        date: `${day.getDate()} ${
+          month[day.getMonth()]
+        } ${day.getFullYear()} ${day.getHours()}:${day.getMinutes()}`,
+      };
+      data.push(obj);
+    }
 
-  return res.status(200).send(data);
+    return res.status(200).send(data);
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
 });
 
